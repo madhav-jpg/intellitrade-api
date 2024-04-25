bc9d40a3e7ac8d5fb97a5645eefe298111b549cc
madhav-jpg <madhavkotecha99@gmail.com>
2024-04-25 16:58:45 +0530

    Secret Key Settings

--- market_api/deployment.py
+++ market_api/deployment.py
@@ -1,6 +1,8 @@
 import os
 from .settings import *
 from .settings import BASE_DIR
+
+SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
@@ -17,7 +19,7 @@ MIDDLEWARE = [
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DATABASES = {
     "default": {
--- market_api/wsgi.py
+++ market_api/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = "market_api.deployment" if "WEBSITE_HOSTNAME" in os.environ else "market_api.settings"
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "market_api.deployment")
 
 application = get_wsgi_application()
